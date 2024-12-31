46ad8c764e1b4e0887fe1140b072f68ca7bf5a95
Yunpeng Yang <yunpeng.yang@gmx.de>
2024-12-31 15:51:00 +0100

    Updated source code with an add function

--- src/app.py
+++ src/app.py
@@ -1,2 +1,6 @@
+def add(a, b):
+    return a + b
+
 if __name__ == "__main__":
     print("Hello, World!")
+    print("Add Function Test: 2 + 3 = ", add(2, 3))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,6 @@
-def test_example():
-    assert 1 + 1 == 2
+ from src.app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
